cc9cf66a4e94777e72f098990bb13781ec5faf7c
Luis Monasterios <ljmm2k2@gmail.com>
2025-01-15 12:14:58 -0400

    Actualizar configuración de ESLint, ajustar exportación en useColorScheme y simplificar tsconfig.json

--- GYMPROMANAGER-APP/.eslintrc.js
+++ GYMPROMANAGER-APP/.eslintrc.js
@@ -1,9 +1,16 @@
 // https://docs.expo.dev/guides/using-eslint/
-module.exports = {
+export default {
   extends: ['expo', 'prettier'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
   },
   ignorePatterns: ['/dist/*'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 }
