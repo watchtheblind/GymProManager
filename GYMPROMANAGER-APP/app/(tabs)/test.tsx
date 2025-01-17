690600bae091000652270fbc4578b998658c9d7d
Luis Monasterios <ljmm2k2@gmail.com>
2025-01-17 15:45:37 -0400

    Agregar componente de casilla de verificación y actualizar formulario de registro con nuevos campos

--- GYMPROMANAGER-APP/app/(tabs)/test.tsx
+++ GYMPROMANAGER-APP/app/(tabs)/test.tsx
@@ -1,6 +1,7 @@
 import Button from '@/components/ui/Button'
 import {router, useNavigation} from 'expo-router'
 import React, {useState} from 'react'
+import checkbox from '@/components/Checkbox'
 import {
   View,
   Text,
@@ -9,8 +10,10 @@ import {
   TextInput,
   Alert,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native'
 import Fetch from '@/hooks/FetchData'
+import Checkbox from 'expo-checkbox'
 
 type FormData = {
   name: string
@@ -51,21 +54,20 @@ export default function Registro() {
         Alert.alert('Error', 'Error de red')
       })
   }
-
+  const [marcado, setMarcado] = React.useState(false)
   return (
-    <View className='bg-[#1D1D1B] h-full text-white relative'>
-      <View className='flex flex-row justify-center'>
+    <View className='flex-1 justify-center h-full text-white relative'>
+      <View className='flex-1 justify-center items-center absolute inset-0 w-full h-full opacity-70'>
         <Image
           className='mt-24'
           style={styles.logo}
           source={require('@/assets/images/logo.png')}
         />
       </View>
+      <View className='absolute inset-0 bg-[#1D1D1B] opacity-90'></View>
       <View className='flex flex-col items-center'>
-        <Text className='text-white text-3xl font-Copperplate'>
-          REGISTRARSE
-        </Text>
-        <View className='w-10/12 flex flex-col gap-5 mt-8'>
+        <Text className='text-white text-3xl font-Copperplate'>REGISTRO</Text>
+        <View className='w-10/12 flex flex-col gap-5 mt-6'>
           <TextInput
             className='bg-[#B0A462] border-4 py-3 border-[#FEF4C9] rounded-tr-3xl rounded-bl-3xl p-2 text-white w-full'
             placeholder='Nombre*'
@@ -95,28 +97,44 @@ export default function Registro() {
             value={formData.email}
             placeholderTextColor='#fff'
           />
+          <TextInput
+            className='bg-[#CC7751] border-4 py-3 border-[#DFAA8C] rounded-tr-3xl rounded-bl-3xl p-2 text-white w-full'
+            placeholder='Número telefónico'
+            onChangeText={(text) => setFormData({...formData, email: text})}
+            value={formData.email}
+            placeholderTextColor='#fff'
+          />
+          <TextInput
+            className='bg-[#B0A462] border-4 py-3 border-[#FEF4C9] rounded-tl-3xl rounded-br-3xl p-2 text-white w-full'
+            placeholder='Email'
+            onChangeText={(text) => setFormData({...formData, email: text})}
+            value={formData.email}
+            placeholderTextColor='#fff'
+          />
+          <TextInput
+            className='bg-[#6CB0B4] border-4 py-3 border-[#518893] rounded-tl-3xl rounded-br-3xl p-2 text-white w-full'
+            placeholder='Contraseña'
+            onChangeText={(text) => setFormData({...formData, password: text})}
+            secureTextEntry={true}
+            value={formData.password}
+            placeholderTextColor='#fff'
+          />
+          <View className='flex flex-row items-center justify-center'>
+            <Checkbox
+              value={marcado}
+              onValueChange={setMarcado}></Checkbox>
+            <Text className='text-white text-xs ml-2 flex-1'>
+              Acepto recibir comunicaciones de Trainingym y puedo consultar la
+              Política de Privacidad
+            </Text>
+          </View>
           <View className='flex flex-row justify-center mt-2'>
             <Button
-              title='Acceder'
+              title='Siguiente '
               onPress={handleSubmit}
             />
           </View>
-          <View className='flex flex-row justify-center'>
-            <TouchableOpacity onPress={redirigirRecuperarContrasena}>
-              <Text className='text-white text-lg underline'>
-                ¿Has olvidado tu contraseña?
-              </Text>
-            </TouchableOpacity>
-          </View>
         </View>
-      </View>
-      <View className='flex flex-row items-center justify-center mt-24'>
-        <Text className='text-white text-lg mr-2'>¿No tienes una cuenta?</Text>
-        <TouchableOpacity onPress={redirigirRegistro}>
-          <Text className='text-[##B0A462] text-lg font-bold underline'>
-            Regístrate aquí
-          </Text>
-        </TouchableOpacity>
       </View>
     </View>
   )
@@ -124,7 +142,7 @@ export default function Registro() {
 
 const styles = StyleSheet.create({
   logo: {
-    height: 150,
-    width: 250,
+    height: 280,
+    width: 380,
   },
 })
