af8b59ef6bc60894924b92636ed02e029593f23b
Luis Monasterios <ljmm2k2@gmail.com>
2025-01-18 15:36:37 -0400

    Actualizar estilos y texto en la vista 3 del carrusel

--- GYMPROMANAGER-APP/app/(tabs)/Carrusel/Vistacarrusel3.tsx
+++ GYMPROMANAGER-APP/app/(tabs)/Carrusel/Vistacarrusel3.tsx
@@ -1,59 +1,46 @@
 import {ThemedText} from '@/components/ThemedText'
-import {Image, StyleSheet, View, useWindowDimensions} from 'react-native'
+import {Image, StyleSheet, View, useWindowDimensions, Text} from 'react-native'
 
 export default function Vistacarrusel3() {
   const {width} = useWindowDimensions()
 
   return (
     <View
-      className='flex flex-col items-center rounded-xl pt-8'
+      className='flex flex-col items-center rounded-6xl pt-8 '
       style={[styles.colorBackground, styles.box]}>
-      <View className='flex flex-col items-center'>
-        <View className='absolute left-0 top-0'>
+      <View className='flex flex-col items-center gap-1'>
+        <View className='absolute left-3 top-0'>
           <Image
             style={styles.polygonContainer}
             source={require('@/assets/images/polygon-01.png')}
           />
         </View>
-        <View className='flex flex-row mb-4 leading-3'>
-          <ThemedText
-            className='text-white'
-            type='title'>
+        <View className='flex flex-row leading-3'>
+          <Text className='text-4xl font-Copperplate text-white'>
             ¿Listo para
-          </ThemedText>
+          </Text>
         </View>
-        <View className='flex flex-row mb-4'>
-          <ThemedText
-            className='mr-2'
-            style={styles.colorRed}
-            type='title'>
-            Transformar
-          </ThemedText>
-          <ThemedText
-            className='mr-2'
-            style={styles.colorWhite}
-            type='title'>
-            tu
-          </ThemedText>
+        <View className='flex flex-row'>
+          <Text className='text-4xl font-Copperplate text-[#DFAA8C]'>
+            transformar
+          </Text>
         </View>
-        <View className='flex flex-row mb-4'>
-          <ThemedText
-            className=''
-            style={styles.colorYellow}
-            type='title'>
+        <View className='flex flex-row'>
+          <Text className='text-4xl font-Copperplate text-white'>tu </Text>
+          <Text className='text-4xl font-Copperplate text-[#B0A462]'>
             negocio?
-          </ThemedText>
+          </Text>
         </View>
         <View
-          className='flex flex-row mb-4 p-8'
+          className='flex flex-row mt-2 mx-1'
           style={{width}}>
-          <ThemedText style={[styles.colorWhite]}>
+          <Text className='text-white text-center text-lg'>
             Con Gym Pro Manager, tendrás acceso a un software de gestión
             avanzado que te permitirá optimizar la administración, la
             comunicación, el cobro y el control de tus operaciones.  
-          </ThemedText>
+          </Text>
         </View>
-        <View className='absolute right-0 bottom-0'>
+        <View className='absolute right-4 top-56'>
           <Image
             style={styles.polygonContainer}
             source={require('@/assets/images/polygon-02.png')}
