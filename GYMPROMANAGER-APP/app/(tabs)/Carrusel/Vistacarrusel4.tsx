81e5f4bed9d4f1730d6118626af07cd2f84322e0
Luis Monasterios <ljmm2k2@gmail.com>
2025-01-18 19:29:32 -0400

    Actualizar la vista 4 del carrusel: simplificar la estructura, mejorar el diseño y ajustar descripciones de membresías

--- GYMPROMANAGER-APP/app/(tabs)/Carrusel/Vistacarrusel4.tsx
+++ GYMPROMANAGER-APP/app/(tabs)/Carrusel/Vistacarrusel4.tsx
@@ -1,154 +1,81 @@
-import {ThemedText} from '@/components/ThemedText'
-import {Image, StyleSheet, View, useWindowDimensions} from 'react-native'
+import React from 'react'
+import {View, useWindowDimensions, Text, ScrollView} from 'react-native'
+import {MaterialCommunityIcons} from '@expo/vector-icons'
 
 export default function Vistacarrusel4() {
   const {width} = useWindowDimensions()
   const plans = [
     {
       id: 1,
-      description: 'GYM PRO MANAGER BRONCE',
-      color: styles.boxRed,
+      description: 'MIEMBRO BRONCE',
+      color: 'bg-[#CC7751]',
+      borderColor: 'border-[#DFAA8C]',
+      borderRadius: 'rounded-tr-3xl rounded-bl-3xl border-4',
       price: 0,
+      icon: 'trophy',
     },
     {
       id: 2,
-      description: 'GYM PRO MANAGER SILVER',
-      color: styles.boxBlue,
+      description: 'MIEMBRO PLATA',
+      color: 'bg-[#518893]',
+      borderColor: 'border-[#6CB0B4]',
+      borderRadius: 'rounded-tl-3xl rounded-br-3xl border-4',
       price: 100,
+      icon: 'medal',
+    },
+    {
+      id: 3,
+      description: 'MIEMBRO ORO',
+      color: 'bg-[#B0A462]',
+      borderColor: 'border-[#FEF4C9]',
+      borderRadius: 'rounded-tr-3xl rounded-bl-3xl border-4',
+      price: 200,
+      icon: 'star',
     },
   ]
+
   return (
-    <View
-      className='relative flex flex-col items-center rounded-xl pt-8'
-      style={[styles.colorBackground, styles.box]}>
-      <View className='relative flex flex-col items-center'>
-        <View className='absolute left-0 top-0'>
-          <Image
-            style={styles.polygonContainer}
-            source={require('@/assets/images/polygon-01.png')}
-          />
-        </View>
-        <View className='flex flex-row mb-4 leading-3'>
-          <ThemedText
-            className='text-white pr-2'
-            type='title'>
-            La
-          </ThemedText>
-          <ThemedText
-            className='pr-2'
-            style={styles.colorRed}
-            type='title'>
-            aplicación
-          </ThemedText>
-          <ThemedText
-            className='text-white'
-            type='title'>
-            que se
-          </ThemedText>
-        </View>
-        <View className='flex flex-row mb-4'>
-          <ThemedText
-            className='mr-2'
-            style={styles.colorBlue}
-            type='title'>
-            ADAPTA
-          </ThemedText>
-          <ThemedText
-            className='mr-2'
-            style={styles.colorWhite}
-            type='title'>
-            a
-          </ThemedText>
-          <ThemedText
-            className='mr-2'
-            style={styles.colorYellow}
-            type='title'>
-            TI
-          </ThemedText>
-        </View>
-        <View className='w-full'>
-          {plans.map((item: any) => {
-            return (
-              <View
-                className='flex flex-col items-center mb-4'
-                style={[styles.card, item.color, {width: width - 30}]}
-                key={item.id}>
-                <ThemedText className='mb-4'>{item.description}</ThemedText>
-                <ThemedText style={styles.sizePrice}>
-                  {item.price} €/mes
-                </ThemedText>
-              </View>
-            )
-          })}
-        </View>
-        <View className='absolute right-0 bottom-0'>
-          <Image
-            style={styles.polygonContainer}
-            source={require('@/assets/images/polygon-02.png')}
-          />
-        </View>
+    <View style={{width, flex: 1}}>
+      <View className='flex flex-row justify-center mt-6'>
+        <Text className='font-Copperplate text-3xl text-white'>LA </Text>
+        <Text className='font-Copperplate text-3xl text-[#DFAA8C]'>
+          APLICACIÓN
+        </Text>
+      </View>
+      <View className='flex flex-row justify-center'>
+        <Text className='font-Copperplate text-3xl text-white'>QUE SE </Text>
+        <Text className='font-Copperplate text-3xl text-[#6CB0B4]'>ADAPTA</Text>
+        <Text className='font-Copperplate text-3xl text-[#B0A462]'> A TI</Text>
+      </View>
+      <View className='flex flex-col flex-1'>
+        <ScrollView
+          showsVerticalScrollIndicator={true}
+          contentContainerStyle={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingBottom: 20,
+          }}
+          className='mt-4 w-full'>
+          {plans.map((item) => (
+            <View
+              key={item.id}
+              className={`p-2 w-80 h-40 items-center mb-4 ${item.color} border-2 ${item.borderColor} ${item.borderRadius}`}>
+              <MaterialCommunityIcons
+                name={item.icon as keyof typeof MaterialCommunityIcons.glyphMap}
+                size={40}
+                color='#fff'
+                className='mb-4'
+              />
+              <Text className='text-xl font-Copperplate text-white text-center mb-2'>
+                {item.description}
+              </Text>
+              <Text className='text-2xl font-Copperplate text-white'>
+                {item.price === 0 ? 'Gratis' : item.price + ' €/mes'}
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  card: {
-    borderTopLeftRadius: 8,
-    borderBottomRightRadius: 8,
-    padding: 4,
-    borderColor: '#ffffff',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    width: '100%',
-    fontSize: 12,
-    lineHeight: 32,
-  },
-  box: {
-    height: 680,
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  imageContainer: {
-    height: 250,
-    backgroundPosition: 'right',
-  },
-  polygonContainer: {
-    height: 45,
-    width: 42,
-  },
-  colorBackground: {
-    backgroundColor: '#1D1D1B',
-  },
-  colorWhite: {
-    color: '#FFFFFF',
-  },
-  colorYellow: {
-    color: '#B0A462',
-  },
-  colorRed: {
-    color: '#DFAA8C',
-  },
-  colorBlue: {
-    color: '#6CB0B4',
-  },
-  defaultText: {
-    fontFamily: 'Copperplate',
-  },
-  boxRed: {
-    backgroundColor: '#DFAA8C',
-  },
-  boxBlue: {
-    backgroundColor: '#6CB0B4',
-  },
-  boxYellow: {
-    backgroundColor: '#B0A462',
-  },
-  sizePrice: {
-    fontSize: 24,
-    lineHeight: 24,
-  },
-})
