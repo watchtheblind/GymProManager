83bafdbb8bae2452b0c2a198624d8e54f5aac6e9
Luis Monasterios <ljmm2k2@gmail.com>
2025-01-23 23:19:51 -0400

    feat: add animations to the components on the home screen

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,3 +1,4 @@
+import React, {useEffect} from 'react'
 import {
   View,
   Text,
@@ -9,44 +10,77 @@ import {
 } from 'react-native'
 import {Settingsicon} from '@/components/ui/Bottomnav/Icons'
 import ButtonImage from '@/components/ui/ButtonImage'
-import {Polygon} from 'react-native-svg'
+import Animated, {
+  FadeInDown,
+  FadeInRight,
+  FadeInLeft,
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withDelay,
+} from 'react-native-reanimated'
+
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity)
 
 const Settingsbutton = () => {
   return (
-    <TouchableOpacity className='flex items-center justify-center mt-10 h-12 w-12 rounded-xl bg-[#518893] border-4 border-[#6CB0B4]'>
+    <AnimatedTouchableOpacity
+      entering={FadeInDown.delay(300).duration(500)}
+      className='flex items-center justify-center mt-10 h-12 w-12 rounded-xl bg-[#518893] border-4 border-[#6CB0B4]'>
       <Settingsicon size={22} />
-    </TouchableOpacity>
+    </AnimatedTouchableOpacity>
   )
 }
 
 export default function Home() {
   const {width} = useWindowDimensions()
+  const logoOpacity = useSharedValue(0)
+
+  useEffect(() => {
+    logoOpacity.value = withDelay(500, withTiming(1, {duration: 1000}))
+  }, [])
+
+  const logoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: logoOpacity.value,
+    }
+  })
+
   return (
     <ScrollView style={styles.container}>
       <View className='p-6'>
         <View className='flex items-end'>
-          <View className='absolute left-6 top-28'>
+          <Animated.View
+            entering={FadeInLeft.delay(200).duration(500)}
+            className='absolute left-6 top-28'>
             <Image
               style={styles.polygonContainer}
               source={require('@/assets/images/polygon-01.png')}
             />
-          </View>
-          <Settingsbutton></Settingsbutton>
-          <View className='absolute right-7 top-56'>
+          </Animated.View>
+          <Settingsbutton />
+          <Animated.View
+            entering={FadeInRight.delay(200).duration(500)}
+            className='absolute right-7 top-56'>
             <Image
               style={[styles.polygonContainer, styles.PolygonRotate]}
               source={require('@/assets/images/polygon-02.png')}
             />
-          </View>
+          </Animated.View>
         </View>
         <View className='gap-3'>
-          <View className='flex flex-row justify-center'>
+          <Animated.View
+            className='flex flex-row justify-center'
+            style={logoStyle}>
             <Image
               style={styles.logo}
               source={require('@/assets/images/logo.png')}
             />
-          </View>
-          <View className='flex flex-col justify-center'>
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.delay(600).duration(500)}
+            className='flex flex-col justify-center'>
             <ButtonImage href='/classes'>
               <Image
                 style={[styles.boderLeft]}
@@ -57,8 +91,10 @@ export default function Home() {
             <Text className='mt-4 text-white text-lg font-Copperplate'>
               CLASES VIRTUALES
             </Text>
-          </View>
-          <View className='flex flex-col justify-center mt-4'>
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.delay(700).duration(500)}
+            className='flex flex-col justify-center mt-4'>
             <ButtonImage href='/activities'>
               <Image
                 style={[styles.boderLeft]}
@@ -69,8 +105,10 @@ export default function Home() {
             <Text className='mt-4 text-white font-Copperplate'>
               ACTIVIDADES EN STREAMING
             </Text>
-          </View>
-          <View className='flex flex-col justify-center mt-4'>
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.delay(800).duration(500)}
+            className='flex flex-col justify-center mt-4'>
             <ButtonImage href='/trainer'>
               <Image
                 style={[styles.boderLeft]}
@@ -81,8 +119,10 @@ export default function Home() {
             <Text className='mt-4 text-white font-Copperplate'>
               ENTRENAMIENTO
             </Text>
-          </View>
-          <View className='flex flex-col justify-center items-center mt-4'>
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.delay(900).duration(500)}
+            className='flex flex-col justify-center items-center mt-4'>
             <View className='flex flex-row w-full gap-2'>
               <View className='flex flex-col items-center w-1/2'>
                 <Image
@@ -105,8 +145,10 @@ export default function Home() {
                 </Text>
               </View>
             </View>
-          </View>
-          <View className='flex flex-col justify-center items-center mt-4 mb-20'>
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.delay(1000).duration(500)}
+            className='flex flex-col justify-center items-center mt-4 mb-20'>
             <View className='flex flex-row w-full gap-2'>
               <View className='flex flex-col items-center w-1/2'>
                 <Image
@@ -127,7 +169,7 @@ export default function Home() {
                 </Text>
               </View>
             </View>
-          </View>
+          </Animated.View>
         </View>
       </View>
     </ScrollView>
