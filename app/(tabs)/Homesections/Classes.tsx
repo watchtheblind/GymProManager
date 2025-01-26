cf519ba74dd88b2a262deab3a61d969108f7eeaa
Luis Monasterios <ljmm2k2@gmail.com>
2025-01-26 14:32:35 -0400

    feat: improve the search functionality on the Classes screen and add an option to clear the search.

--- app/(tabs)/Homesections/Classes.tsx
+++ app/(tabs)/Homesections/Classes.tsx
@@ -8,16 +8,18 @@ import {
   ScrollView,
   KeyboardAvoidingView,
   TextInput,
+  TouchableOpacity,
 } from 'react-native'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import {Stack} from 'expo-router'
 import {useFetchAPI} from '@/hooks/useFetchAPI'
 import {ThemedText} from '@/components/ThemedText'
 import Settingsbutton from '@/components/ui/Settingsbutton'
+
 export default function Classes() {
   const [search, setSearch] = useState('')
   const {data, loading, error} = useFetchAPI('/clases', {})
-  const [searchedClasess, setSearchedClasses] = useState<any[]>()
+  const [searchedClasses, setSearchedClasses] = useState<any[]>()
 
   const colors = ['#CC7751', '#518893', '#B0A462']
   const nivels = ['Principiante', 'Intermedio', 'Experto']
@@ -43,15 +45,29 @@ export default function Classes() {
   const setSearchTextAlways = (text: string) => {
     setSearch(text)
   }
+
   const searchClass = async (text: string) => {
     await fakeDelay(1000)
-    const validationSource = data.filter((item: any) => item.titulo)
-    const filteredClasess = validationSource.filter((item: any) => {
-      return item.titulo.toLowerCase().includes(text.toLowerCase())
-    })
-    setSearchedClasses(filteredClasess)
+    if (text.trim() === '') {
+      setSearchedClasses(data)
+    } else {
+      const validationSource = data.filter((item: any) => item.titulo)
+      const filteredClasses = validationSource.filter((item: any) => {
+        return item.titulo.toLowerCase().includes(text.toLowerCase())
+      })
+      setSearchedClasses(filteredClasses)
+    }
   }
+
   const debounceSearchClass = debounce(searchClass, setSearchTextAlways, 1000)
+
+  const clearSearch = () => {
+    setSearch('')
+    setSearchedClasses(data)
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+  }
 
   useEffect(() => {
     setSearchedClasses(data)
@@ -61,7 +77,7 @@ export default function Classes() {
     return () => {
       clearTimeout(timeout)
     }
-  }, [])
+  }, [timeout]) // Added timeout to the dependency array
 
   return (
     <ScrollView
@@ -75,7 +91,7 @@ export default function Classes() {
           }}
         />
         <View className='flex flex-row items-center justify-between'>
-          <Text className=' text-white text-xl font-Copperplate '>
+          <Text className='text-white text-xl font-Copperplate'>
             CLASES VIRTUALES
           </Text>
           <Settingsbutton></Settingsbutton>
@@ -90,8 +106,9 @@ export default function Classes() {
               keyboardType='default'
               placeholder='Buscar'
               placeholderTextColor={'#fff'}
+              value={search}
               onChangeText={debounceSearchClass}
-              className='pl-12 py-2  rounded-bl-3xl rounded-tr-3xl bg-[#B0A462] border-2 border-solid border-[#FEF4C9]'
+              className='pl-12 pr-10 py-2 rounded-bl-3xl rounded-tr-3xl bg-[#B0A462] border-2 border-solid border-[#FEF4C9]'
               style={{
                 fontSize: 16,
                 fontFamily: 'Myriad Pro',
@@ -105,13 +122,24 @@ export default function Classes() {
                 color='white'
               />
             </View>
+            {search !== '' && (
+              <TouchableOpacity
+                onPress={clearSearch}
+                className='absolute right-3'>
+                <MaterialIcons
+                  name='close'
+                  size={24}
+                  color='white'
+                />
+              </TouchableOpacity>
+            )}
           </View>
         </KeyboardAvoidingView>
         <View className='flex flex-row w-full'>
           <View className='flex flex-row w-full flex-wrap mb-20'>
-            {searchedClasess &&
+            {searchedClasses &&
               Array.from({length: 3}).map((_, outerIndex) =>
-                searchedClasess.map((item: any, index: number) => {
+                searchedClasses.map((item: any, index: number) => {
                   return (
                     <View
                       className='flex flex-col items-left w-[50%] px-2 py-4'
@@ -149,6 +177,7 @@ export default function Classes() {
     </ScrollView>
   )
 }
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#1D1D1B',
