ffa0c4b0e98cc1324378699985df6cf312bac4af
Luis Monasterios <ljmm2k2@gmail.com>
2025-01-31 15:55:04 -0400

    refactor: Redesign login component to add loading state and loader animations to fields and button

--- app/(tabs)/Login.tsx
+++ app/(tabs)/Login.tsx
@@ -6,6 +6,7 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
+  ActivityIndicator,
 } from 'react-native'
 import Icon from '@expo/vector-icons/MaterialIcons'
 import Button from '@/components/ui/Button'
@@ -20,13 +21,17 @@ export default function Login() {
   const [showPassword, setShowPassword] = useState(false)
   const [alertVisible, setAlertVisible] = useState(false) // Estado para controlar la visibilidad de la alerta
   const [alertMessage, setAlertMessage] = useState('') // Mensaje de la alerta
+  const [isLoading, setIsLoading] = useState(false) // Estado para controlar el loader
 
   const redirectToRegister = useCallback(() => {
     router.navigate('./Signup')
   }, [])
 
   const handleSubmit = useCallback(async () => {
+    setIsLoading(true) // Activar el estado de carga
     try {
+      // Simulación de un retraso de 5 segundos
+      await new Promise((resolve) => setTimeout(resolve, 5000))
       const response = await fetch('https://gympromanager.com/app-login.php', {
         method: 'POST',
         headers: {
@@ -34,9 +39,7 @@ export default function Login() {
         },
         body: `email=${encodeURIComponent(formData.email)}&password=${encodeURIComponent(formData.password)}`,
       })
-
       const data = await response.json()
-
       if (data.user_email) {
         router.navigate('./Bottomnav')
       } else if (data.error) {
@@ -52,6 +55,8 @@ export default function Login() {
       console.error(error)
       setAlertMessage('Error de red inesperado. Por favor, inténtelo de nuevo.') // Establece el mensaje de error
       setAlertVisible(true) // Muestra la alerta
+    } finally {
+      setIsLoading(false) // Desactivar el estado de carga
     }
   }, [formData])
 
@@ -72,17 +77,27 @@ export default function Login() {
       <View className='flex flex-col items-center z-10 mt-20'>
         <Text className='text-white text-3xl font-Copperplate'>ACCEDER</Text>
         <View className='w-10/12 flex flex-col gap-5 mt-8'>
+          {/* Campo de correo electrónico */}
           <TextInput
-            className='bg-[#B0A462] border-4 py-3 border-[#FEF4C9] rounded-tr-3xl rounded-bl-3xl p-2 text-white'
+            className={`${
+              isLoading ? 'bg-[#555]' : 'bg-[#B0A462]'
+            } border-4 py-3 rounded-tr-3xl rounded-bl-3xl p-2 text-white`}
             placeholder='Correo electrónico'
             onChangeText={(text) => setFormData({...formData, email: text})}
             value={formData.email}
             placeholderTextColor='#fff'
-            style={styles.input}
+            editable={!isLoading} // Desactivar cuando isLoading es true
+            style={[
+              styles.input,
+              {borderColor: isLoading ? '#555' : '#FEF4C9'}, // Cambiar el color del borde según isLoading
+            ]}
           />
+          {/* Campo de contraseña */}
           <View style={styles.passwordContainer}>
             <TextInput
-              className='bg-[#6CB0B4] border-4 py-3 border-[#518893] rounded-tl-3xl rounded-br-3xl p-2 text-white'
+              className={`${
+                isLoading ? 'bg-[#444]' : 'bg-[#6CB0B4]'
+              } border-4 py-3 rounded-tl-3xl rounded-br-3xl p-2 text-white`}
               placeholder='Contraseña'
               onChangeText={(text) =>
                 setFormData({...formData, password: text})
@@ -90,7 +105,11 @@ export default function Login() {
               secureTextEntry={!showPassword}
               value={formData.password}
               placeholderTextColor='#fff'
-              style={styles.passwordInput}
+              editable={!isLoading} // Desactivar cuando isLoading es true
+              style={[
+                styles.passwordInput,
+                {borderColor: isLoading ? '#444' : '#518893'}, // Cambiar el color del borde según isLoading
+              ]}
             />
             <TouchableOpacity
               onPress={() => setShowPassword(!showPassword)}
@@ -102,12 +121,31 @@ export default function Login() {
               />
             </TouchableOpacity>
           </View>
+          {/* Botón Acceder con loader */}
           <View className='flex flex-row justify-center mt-2'>
             <Button
-              title='Acceder'
+              title={isLoading ? '' : 'Acceder'} // Cambiar texto del botón
               onPress={handleSubmit}
+              disabled={isLoading} // Desactivar el botón mientras isLoading es true
+              style={[
+                styles.button,
+                {
+                  backgroundColor: isLoading ? '#555' : '#B0A462',
+                  borderColor: isLoading ? '#555' : '#FEF4C9', // Cambiar el color del borde según isLoading
+                },
+              ]}
+              icon={
+                isLoading && (
+                  <ActivityIndicator
+                    size='small'
+                    color='#fff'
+                    style={{marginLeft: 8}}
+                  />
+                )
+              }
             />
           </View>
+          {/* Enlace para recuperar contraseña */}
           <View className='flex flex-row justify-center'>
             <TouchableOpacity
               onPress={() => {
@@ -125,6 +163,7 @@ export default function Login() {
           </View>
         </View>
       </View>
+      {/* Enlace para registrarse */}
       <View className='flex flex-row items-center justify-center mt-28'>
         <Text
           className='text-white text-xl mr-2'
@@ -134,12 +173,11 @@ export default function Login() {
           ¿No tienes una cuenta?
         </Text>
         <TouchableOpacity onPress={redirectToRegister}>
-          <Text className='text-[##B0A462] text-lg font-bold underline'>
+          <Text className='text-[#B0A462] text-lg font-bold underline'>
             Regístrate aquí
           </Text>
         </TouchableOpacity>
       </View>
-
       {/* Componente CustomAlert */}
       <CustomAlert
         visible={alertVisible}
@@ -172,5 +210,11 @@ const styles = StyleSheet.create({
   passwordInput: {
     width: '100%',
     paddingRight: 40,
+  },
+  button: {
+    borderWidth: 2,
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
   },
 })
