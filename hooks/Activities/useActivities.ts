0f1efb6e6abc3306b63c71bdd8dbe085026435c8
Luis Monasterios <ljmm2k2@gmail.com>
2025-01-31 21:03:59 -0400

    refactor[API]: use original activities endpoint

--- hooks/Activities/useActivities.ts
+++ hooks/Activities/useActivities.ts
@@ -1,13 +1,14 @@
 import {useState, useEffect} from 'react'
 import moment from 'moment'
 
-const API_URL = 'https://retoolapi.dev/ugkMJ5/GYMACTIVITIESAPP'
+const API_URL = 'https://gympromanager.com/app-activities.php'
+const TOKEN = 'Contraseña...'
 
 interface Activity {
-  id: string
+  id: number
   name: string
   time: string
-  type: 'yoga' | 'cardio' | 'pilates' | 'strength' | 'dance'
+  type: string
   capacity: number
   available: number
   Instructor: string
@@ -23,20 +24,38 @@ export const useActivities = (selectedDate: Date) => {
       try {
         setLoading(true)
         setError(null)
-        const formattedDate = moment(selectedDate).format('YYYY-MM-DD')
-        const response = await fetch(API_URL)
+
+        const response = await fetch(API_URL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: `token=${TOKEN}`,
+        })
+
         if (!response.ok) throw new Error('Failed to fetch activities')
+
         const data = await response.json()
+
+        // Filtra las actividades por la fecha seleccionada
+        const formattedDate = moment(selectedDate).format('YYYY-MM-DD')
         const filteredData = data.filter(
           (activity: Activity) =>
             moment(activity.time, 'MMM DD, YYYY h:mm A').format(
               'YYYY-MM-DD',
             ) === formattedDate,
         )
+
         setActivities(filteredData)
-      } catch (error) {
-        console.error('Error fetching activities:', error)
-        setError('Failed to load activities. Please try again.')
+      } catch (error: any) {
+        console.error('Error al cargar actividades:', error.message)
+        setError(
+          'Ocurrió un error al cargar las actividades.' +
+            '\n' +
+            '(' +
+            error.message +
+            ')',
+        )
       } finally {
         setLoading(false)
       }
