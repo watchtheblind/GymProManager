d532f7859f399ceb3e5f60ffec8b1a5090b349b6
Luis Monasterios <ljmm2k2@gmail.com>
2025-02-05 16:55:20 -0400

    `Added HapticTab, TabBarBackground, and color scheme dependent tint to TabLayout`

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,20 @@
 import {Tabs} from 'expo-router'
 import React from 'react'
-
+import {HapticTab} from '@/components/HapticTab'
+import TabBarBackground from '@/components/ui/TabBarBackground'
+import {Colors} from '@/constants/Colors'
 import {useColorScheme} from '@/hooks/useColorScheme'
 
 export default function TabLayout() {
+  const colorScheme = useColorScheme()
+
   return (
     <Tabs
       screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
         tabBarStyle: {
           display: 'none',
         },
