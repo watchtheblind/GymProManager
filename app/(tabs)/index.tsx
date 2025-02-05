19f581ad66446f25135fd287e6c21f05338f4daf
Luis Monasterios <ljmm2k2@gmail.com>
2025-02-05 18:47:14 -0400

    refactor: make app go Home if session is active

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,10 @@
 import Carousel from './Carousel/Carousel'
+import BottomTabs from './Bottomnav'
+import {useSession} from '@/hooks/SessionContext'
 export default function HomeScreen() {
-  return <Carousel></Carousel>
+  const {user: activeSession} = useSession()
+  if (!activeSession) {
+    return <Carousel></Carousel>
+  }
+  return <BottomTabs></BottomTabs>
 }
