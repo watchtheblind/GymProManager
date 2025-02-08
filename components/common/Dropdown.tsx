062ab78981348a4bc443a1842449f416344d4381
Luis Monasterios <ljmm2k2@gmail.com>
2025-02-08 19:21:07 -0400

    feat: add dropdown menu for selecting height and weight values
    redo height and weight fields so it can be passed with it's value and unit type to the endpoint

---
+++ components/common/Dropdown.tsx
@@ -0,0 +1,97 @@
+'use client'
+
+import type React from 'react'
+import {useState} from 'react'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Modal,
+  FlatList,
+  StyleSheet,
+} from 'react-native'
+
+interface DropdownProps {
+  options: string[]
+  selectedValue: string
+  onSelect: (value: string) => void
+  placeholder?: string
+}
+
+const Dropdown: React.FC<DropdownProps> = ({
+  options,
+  selectedValue,
+  onSelect,
+  placeholder,
+}) => {
+  const [modalVisible, setModalVisible] = useState(false)
+
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={() => setModalVisible(true)}
+        style={styles.dropdownButton}>
+        <Text>{selectedValue || placeholder || 'Select'}</Text>
+      </TouchableOpacity>
+      <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType='fade'
+        onRequestClose={() => setModalVisible(false)}>
+        <TouchableOpacity
+          style={styles.modalOverlay}
+          activeOpacity={1}
+          onPress={() => setModalVisible(false)}>
+          <View style={styles.modalContent}>
+            <FlatList
+              data={options}
+              keyExtractor={(item) => item}
+              renderItem={({item}) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    onSelect(item)
+                    setModalVisible(false)
+                  }}
+                  style={styles.optionItem}>
+                  <Text className='text-white text-center'>{item}</Text>
+                </TouchableOpacity>
+              )}
+            />
+          </View>
+        </TouchableOpacity>
+      </Modal>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  dropdownButton: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 8,
+    padding: 5,
+    marginRight: 3,
+    minWidth: 'auto',
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    color: '#ffffff',
+  },
+  modalContent: {
+    backgroundColor: '#1D1D1B',
+    borderRadius: 8,
+    padding: 10,
+    color: '#ffffff',
+    width: '20%',
+    maxHeight: '40%',
+  },
+  optionItem: {
+    padding: 16,
+    color: '#ffffff',
+  },
+})
+
+export default Dropdown
