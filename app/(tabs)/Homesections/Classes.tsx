d228553870cd083fd3badd0b68dc514651fd6484
Luis Monasterios <ljmm2k2@gmail.com>
2025-02-15 21:04:30 -0400

    feat: add video player modal and updated Classes component to display thumbnails and play videos

--- app/(tabs)/Homesections/Classes.tsx
+++ app/(tabs)/Homesections/Classes.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {
   View,
   StyleSheet,
@@ -6,7 +6,10 @@ import {
   Text,
   ActivityIndicator,
   FlatList,
+  Modal,
+  TouchableOpacity,
 } from 'react-native'
+import {WebView} from 'react-native-webview'
 import {Stack} from 'expo-router'
 import {fetchEjercicios} from '@/hooks/Data/Endpoints'
 import {useNavigation} from '@react-navigation/native'
@@ -32,6 +35,7 @@ export default function Classes() {
   const [loading, setLoading] = useState<boolean>(true)
   const [error, setError] = useState<string | null>(null)
   const [thumbnails, setThumbnails] = useState<{[key: string]: string}>({})
+  const [selectedVideo, setSelectedVideo] = useState<string | null>(null) // URL del video seleccionado
   const colors = ['#CC7751', '#518893', '#B0A462']
   const nivels = ['Principiante', 'Intermedio', 'Experto']
   const colorNivel = (nivel: number) => colors[nivel - 1]
@@ -97,7 +101,6 @@ export default function Classes() {
         }))
         setEjercicios(decodedData)
         setSearchedEjercicios(decodedData)
-
         const thumbnailPromises = decodedData.map(async (item) => {
           const {platform, videoId} = extractVideoIdAndPlatform(item.url)
           if (platform === 'vimeo' && videoId) {
@@ -159,7 +162,6 @@ export default function Classes() {
         onSearch={searchClass}
         onClear={clearSearch}
       />
-
       <FlatList
         data={searchedEjercicios}
         keyExtractor={(item) => item.ID}
@@ -180,11 +182,31 @@ export default function Classes() {
               duration='GymPro'
               isFavorite={false}
               showFavoriteIcon={false}
-              onPress={() => {}}
+              onPress={() => setSelectedVideo(item.url)} // Abrir el video seleccionado
             />
           )
         }}
       />
+
+      {/* Modal para el reproductor de video */}
+      <Modal
+        visible={!!selectedVideo}
+        animationType='slide'
+        onRequestClose={() => setSelectedVideo(null)}>
+        <View style={styles.modalContainer}>
+          <WebView
+            source={{uri: selectedVideo || ''}}
+            style={styles.webview}
+            allowsFullscreenVideo={true} // Habilita pantalla completa
+            mediaPlaybackRequiresUserAction={false} // Habilita autoplay
+          />
+          <TouchableOpacity
+            onPress={() => setSelectedVideo(null)}
+            style={styles.closeButton}>
+            <Text style={styles.closeButtonText}>Cerrar</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </SafeAreaView>
   )
 }
@@ -219,5 +241,26 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between', // Añade espacio entre las columnas
     alignItems: 'stretch',
     paddingBlock: 5,
+  },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: '#1D1D1B',
+  },
+  webview: {
+    flex: 1,
+    backgroundColor: '#1D1D1B',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    padding: 10,
+    backgroundColor: '#5D5D5B',
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    color: '#000',
+    fontFamily: 'MyriadPro',
+    fontWeight: '500',
   },
 })
